8fa6938e0c955c64d2d10f02fba5195b8367e2a9
Stiflerzak <hilarymwendia97@gmail.com>
2023-05-15 18:31:59 +0300

    Booklist

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import light from './images/light.jpg'
+import robert from './images/robert.jpg'
+import rich from './images/rich.jpg'
+
+export const books = [
+  {
+    author: "Michelle Obama",
+    title: "The Light We Carry: Overcoming in Uncertain Times",
+    img:light,
+    id: 1,
+  },
+  {
+    author: "Ramit Sethi",
+    title:"I Will Teach You to Be Rich: No Guilt. No Excuses. Just a 6-Week Program That Works (Second Edition)",
+    img: rich,
+    id: 2,
+  },
+  {
+    author: "Robert Kiyosaki",
+    title: "The Rich Dad Poor Dad",
+    img: robert,
+    id: 3,
+  },
+];
